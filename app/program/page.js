25988a4d30f1ec313515d4238fd04062cb900889
Robert Von Der Becke <vonderbeckememberships@gmail.com>
2023-06-13 10:50:23 -0500

    added missing link for tkd

--- app/program/page.js
+++ app/program/page.js
@@ -1,4 +1,5 @@
 import "./program.css";
+import Link from "next/link";
 export default function Program() {
   return (
     <>
@@ -20,8 +21,7 @@ export default function Program() {
               build confidence in our practitioners while building skills that
               can be demonstrated when needed. Whether you want to compete with
               our team at the highest levels or just reach your full potential
-              this is the school for you and your family.
-              Our
+              this is the school for you and your family. Our
               poomsae,self-defense techniques and sparring may be rooted from a
               long line of masters. Our program however is not stuck in the
               past. Our kids program deleiver a fun and engaging curruculuim
@@ -30,9 +30,11 @@ export default function Program() {
               the playing feild allowing diverse learner to exceed expectaions
               on there journey to become black belt practitioners.
             </p>
-            <button className="btn programsBtn programsBtnRight">
-              Sign up Now
-            </button>
+            <Link href="/enroll">
+              <button className="btn programsBtn programsBtnRight">
+                Sign up Now
+              </button>
+            </Link>
           </div>
         </div>
         <div className="programsFooter programsFooterRed">
